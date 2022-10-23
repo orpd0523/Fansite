bbd0384e34affbf43ec48af8389280b4e0ff3e8d
orpd0523 <orpd0523@gmail.com>
2022-10-23 15:21:13 -0500

    created display comment function

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,28 +1,69 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    //console.log('Window\'s content fully loaded.')
-    let commentForm = document.querySelector(".comments__form");
-    commentForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        //console.log("Form Submitted")
-        //console.log(event.target[0].value);
-        //console.log(event.target[1].value);
-        let commentObject = {
-            username: event.target[0].value,
-            comment: event.target[1].value
-        };
-        console.log(commentObject);
-    });
+document.addEventListener("DOMContentLoaded", (event) => {
+  //console.log('Window\'s content fully loaded.')
+  let testComment = {
+    name: "Miles Acosta",
+    timestamp: "12/20/2020",
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  };
+  displayComment(testComment)
+
+
+
+
+  let commentForm = document.querySelector(".comments__form");
+  commentForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    //console.log("Form Submitted")
+    //console.log(event.target[0].value);
+    //console.log(event.target[1].value);
+    let commentObject = {
+      username: event.target[0].value,
+      comment: event.target[1].value,
+    };
+    console.log(commentObject);
+  });
 });
 
+let defaultComments = [
+  {
+    name: "Connor Walton",
+    timestamp: "02/17/2021",
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    timestamp: "01/09/2021",
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    timestamp: "12/20/2020",
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
+function displayComment(commentObject) {
+    console.log(commentObject);
+    let display = document.querySelector('.comments__display');
+   
+    let container = document.createElement ("div");
+    display.appendChild(container);
+
+    let user = document.createElement ("p");
+    user.innerText = commentObject.name;
+    container.appendChild(user);
 
 
+    let date = document.createElement ("p");
+    date.innerText = commentObject.timestamp;
+    container.appendChild(date);
 
 
-
-
-
-
-
-
-
-
+    let paragraph = document.createElement ("p");
+    paragraph.innerText = commentObject.comment;
+    container.appendChild(paragraph);
+}
