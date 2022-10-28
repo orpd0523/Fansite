32fdb6d975d7e1369e08b7994ee4b3586e442955
orpd0523 <orpd0523@gmail.com>
2022-10-28 00:24:39 -0500

    accessed data from API

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,5 +1,8 @@
+const BASE_URL = "http://project-1-api.herokuapp.com/"
+let apikey = "?api_key=d10b7838-1997-4fb9-bf66-f0cdd4a312ba"
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  renderComments();
+  getComments();
   let commentForm = document.querySelector(".comments__form");
   commentForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -13,31 +16,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
     comments.push(commentObject);
     commentForm.reset();
     clearComments();
-    renderComments();
+    getComments();
     
   });
 });
 
-let comments = [
-  {
-    name: "Miles Acosta",
-    timestamp: "12/20/2020",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-  {
-    name: "Emilie Beach",
-    timestamp: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Connor Walton",
-    timestamp: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-];
 
 function displayComment(commentObject) {
   let display = document.querySelector(".comments__display");
@@ -82,11 +65,15 @@ function displayComment(commentObject) {
   cardMain.appendChild(message);
 }
 
-function renderComments() {
-  
-  comments.reverse().forEach((comment) => {
+function renderComments(comments) {
+  comments.forEach((comment) => {
     displayComment(comment);
   });
+}
+
+function getComments(){
+  axios.get(BASE_URL + "comments"+ apikey )
+  .then((response) => {renderComments(response.data)}); 
 }
 
 function clearComments() {
